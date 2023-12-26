878e1c7f395ecf74109db8839514750e3a05f7e9
Sona <s.harutyunyan1992@gmail.com>
2023-12-26 23:35:03 +0400

    Задания выполненны

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,61 @@
-﻿
+﻿//Задание 1
+
+function parseCount(value) {
+    const parsedValue = parseFloat(value);
+    
+    if (isNaN(parsedValue)) {
+        throw new Error("Невалидное значение");
+    }
+    
+    return parsedValue;
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch (error) {
+        return error;
+    }
+}
+
+
+
+//Задание 2
+
+class Triangle {
+	constructor(a,b,c){
+		this.a= a;
+		this.b= b;
+		this.c=c;
+		    if(a+b<c 
+            || b+c<a 
+            || a+c<b){
+			throw new Error("Треугольник с такими сторонами не существует")
+		}
+    }
+		 get perimeter() {
+        return this.a + this.b + this.c;
+    }
+    get area() {
+        const p = this.perimeter / 2;
+        return parseFloat((+Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(3));
+        
+    }
+}
+
+function getTriangle(a,b,c){
+	try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        return {
+            get area() {
+                return "Ошибка! Треугольник не существует";
+            },
+            get perimeter() {
+                return "Ошибка! Треугольник не существует";
+            }
+        }
+    }
+}
+
+
