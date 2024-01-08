e1419ff20f8e866208ed5deb64b74abbcf80357b
Sona <s.harutyunyan1992@gmail.com>
2024-01-08 16:25:01 +0400

    Задание выполнено

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,59 @@
 //Задача № 1
+// Импортируем библиотеку для вычисления MD5 хеша
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+
+    function wrapper(...args) {
+        const hash = md5(JSON.stringify(args));
+
+
+        let objectInCache = cache.find((item) => item.hash === hash);
+
+        if (objectInCache) {
+            console.log("Из кеша: " + objectInCache.result);
+            return "Из кеша: " + objectInCache.result;
+        }
+
+
+        const result = func(...args);
+
+
+        cache.push({hash, result});
+
+        if (cache.length > 5) {
+            cache.shift();
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;
+    }
+
+    return wrapper;
 }
+
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+
+    let timeoutId = null;
+
+    function wrapper(...args) {
+        clearTimeout(timeoutId);
+
+        if (timeoutId === null) {
+            func(...args);
+            wrapper.count++;
+        }
+        timeoutId = setTimeout(() => {
+            func(...args);
+            wrapper.count++;
+        }, delay);
+
+        wrapper.allCount++;
+
+    }
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+    clearTimeout(timeoutId);
+
+    return wrapper;
 }
